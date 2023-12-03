f77f926f4feb6f49a7d805d2ae5efd5940f87319
Matheus <matheusb.comp@gmail.com>
2023-12-03 00:28:29 -0300

    wip: IndexedDB audio storage

--- src/public/js/index.js
+++ src/public/js/index.js
@@ -1,28 +1,87 @@
 import SoundPlayer from "./player.js";
+import SoundStorage from "./storage.js";
 
+//
+const btnSaveFiles = document.getElementById("btn-save-files");
+const fileNameList = document.getElementById("list-uploaded-files");
+const inputFiles = document.getElementById("upload-sound-files");
+
+//
+inputFiles.addEventListener("change", (ev) => {
+  fileNameList.innerHTML = "";
+  btnSaveFiles.disabled = true;
+
+  //
+  const files = ev.target.files;
+  for (const f of files) {
+    let p = document.createElement("p");
+    p.innerText = `${f.name} (${f.type})\n -> size: ${Math.floor(
+      f.size / 1024
+    )}KB`;
+    fileNameList.appendChild(p);
+  }
+
+  //
+  if (files.length) btnSaveFiles.disabled = false;
+});
+
+//
+btnSaveFiles.addEventListener("click", (ev) => {
+  const btn = ev.target;
+  if (!inputFiles?.files?.length) return;
+
+  // TODO: Implement real indexedDB "save" async method
+  const fakeSave = () => new Promise((res, rej) => {
+    const ms = Math.floor(Math.random() * 5000);
+    setTimeout(() => ms <= 500 ? rej(new Error('FAKE ERROR')) : res(), ms);
+  });
+
+  //
+  const label = btn.innerText;
+  btn.innerText = "Salvando...";
+  btn.disabled = true;
+  inputFiles.disabled = true;
+  fakeSave(inputFiles.files)
+    .then(() => {
+      fileNameList.innerHTML = "";
+      inputFiles.value = "";
+    })
+    .catch((err) => {
+      console.error(err);
+      btn.disabled = false;
+    })
+    .finally(() => {
+      btn.innerText = label;
+      inputFiles.disabled = false;
+    });
+});
+
+// TODO: Organize
 const sp = new SoundPlayer();
+const ss = new SoundStorage();
 window.sp = sp;
+window.ss = ss;
 
-const audioPing = document.getElementById("ping");
+// const audioPing = document.getElementById("ping");
 
-const playSound = (filePath) => {
-  audioPing.src = filePath;
-  audioPing.load();
-  audioPing.play();
-};
+// const playSound = (filePath) => {
+//   audioPing.src = filePath;
+//   audioPing.load();
+//   audioPing.play();
+// };
 
-document.getElementById("btn_ping_play").addEventListener("click", (ev) => {
-  audioPing.play();
-});
+// document.getElementById("btn_ping_play").addEventListener("click", (ev) => {
+//   audioPing.play();
+// });
 
-document.getElementById("btn_ping_pause").addEventListener("click", (ev) => {
-  audioPing.pause();
-});
+// document.getElementById("btn_ping_pause").addEventListener("click", (ev) => {
+//   audioPing.pause();
+// });
 
-document.getElementById("btn_sound_1").addEventListener("click", (ev) => {
-  playSound("media/test_sound/ping.mp3");
-});
+// document.getElementById("btn_sound_1").addEventListener("click", (ev) => {
+//   playSound("media/test_sound/ping.mp3");
+// });
 
-document.getElementById("btn_sound_2").addEventListener("click", (ev) => {
-  playSound("media/test_sound/drop.mp3");
-});
+// document.getElementById("btn_sound_2").addEventListener("click", (ev) => {
+//   playSound("media/test_sound/drop.mp3");
+// });
---
+++ src/public/js/storage.js
@@ -0,0 +1,112 @@
+class SoundStorage {
+  /**
+   * @type {IDBDatabase | null}
+   */
+  #idb;
+  #dbName;
+  #storeName;
+
+  constructor() {
+    this.#idb = null;
+    this.#dbName = "soundLibrary";
+    this.#storeName = "soundFiles";
+  }
+
+  get #db() {
+    if (!this.#idb) throw new Error("Please call setup first");
+    return this.#idb;
+  }
+
+  setup = async () => {
+    this.#idb = await this.#open(this.#dbName);
+    this.#idb.addEventListener("error", (err) => {
+      console.error("SoundStorage error:", err);
+    });
+  };
+
+  findFile = (id) =>
+    new Promise((resolve, reject) => {
+      // Transaction handlers
+      const tx = this.#db.transaction([this.#storeName], "readonly");
+      tx.addEventListener("error", (ev) => reject(ev.target.error));
+
+      // Request: Get a single object by ID
+      tx.objectStore(this.#storeName)
+        .get(id)
+        .addEventListener("success", (ev) => resolve(ev.target.result));
+    });
+
+  addFilesToBucket = (bucket, files, extraData = []) =>
+    new Promise((resolve, reject) => {
+      let res = [];
+
+      // Transaction handlers
+      const tx = this.#db.transaction([this.#storeName], "readwrite");
+      tx.addEventListener("error", (ev) => reject(ev.target.error));
+      tx.addEventListener("complete", () => resolve(res));
+
+      // Requests: Add each file and save the returned ID
+      const store = tx.objectStore(this.#storeName);
+      Array.from(files).forEach((file, i) =>
+        store.add({ bucket, file }).addEventListener("success", (ev) =>
+          res.push({
+            id: ev.target.result,
+            bucket,
+            file,
+            extra: extraData?.[i] || {},
+          })
+        )
+      );
+    });
+
+  // TODO: Implement "Update files" (change bucket or file, needs to pass an ID)
+
+  // TODO: Implement "Delete files" (single ID or entire bucket)
+
+  /**
+   *
+   * @param {string} dbName
+   * @param {number | undefined} version
+   * @returns {Promise<IDBDatabase>}
+   */
+  #open = (dbName, version) =>
+    new Promise((resolve, reject) => {
+      const req = indexedDB.open(dbName, version);
+      req.addEventListener("error", (ev) => reject(ev.target.error));
+      req.addEventListener("success", (ev) => resolve(ev.target.result));
+      req.addEventListener("upgradeneeded", (ev) => {
+        const { target, newVersion, oldVersion } = ev;
+        const db = this.#upgrade(target.result, newVersion, oldVersion);
+        resolve(db);
+      });
+    });
+
+  /**
+   *
+   * @param {IDBDatabase} db
+   * @param {number} newVersion
+   * @param {number} oldVersion
+   * @returns {Promise<IDBDatabase>}
+   */
+  #upgrade = (db, newVersion, oldVersion) =>
+    new Promise((resolve) => {
+      if (newVersion > 1) throw new Error(`Invalid IDB version ${newVersion}`);
+      console.debug(`SoundStorage IDB upgrade: ${oldVersion} => ${newVersion}`);
+
+      /**
+       * The object store will hold files with the structure:
+       * `{ bucket, name, type, size, lastModified, bytes (blob) }`
+       * Where the `bucket` is used to make separate "lists of files"
+       */
+      const store = db.createObjectStore(this.#storeName, {
+        keyPath: "id",
+        autoIncrement: true,
+      });
+      store.createIndex("bucket", "bucket", { unique: false });
+
+      // Resolve the promise when the object store creation completes
+      store.transaction.addEventListener("complete", () => resolve(db));
+    });
+}
+
+export default SoundStorage;
